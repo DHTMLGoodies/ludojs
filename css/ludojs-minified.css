fc04a55ae303fe3ef962673699398ebd4eb6cb45
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-22 01:00:49 +0100

    Fixed path to light gray skin

--- css/ludojs-minified.css
+++ css/ludojs-minified.css
@@ -1,4 +1,4 @@
-/* Generated Fri Feb 22 0:42:24 CET 2013 */
+/* Generated Fri Feb 22 0:53:25 CET 2013 */
 /************************************************************************************************************
 @fileoverview
 ludoJS - Javascript framework
