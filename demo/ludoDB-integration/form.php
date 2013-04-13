38fc1e38564630964b6d5fa7d768980bf0245acf
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-04-13 22:24:33 +0200

    ludoDB js component built into ludojs.js

--- demo/ludoDB-integration/form.php
+++ demo/ludoDB-integration/form.php
@@ -4,13 +4,6 @@ $pageTitle = 'LudoDB Integration example';
 require_once("../includes/demo-header.php");
 ?>
 <body>
-<script type="text/javascript" src="<?php echo $prefix; ?>../src/view.js"></script>
-<script type="text/javascript" src="<?php echo $prefix; ?>../src/layout/card.js"></script>
-<script type="text/javascript" src="<?php echo $prefix; ?>../src/ludoDB/factory.js"></script>
-<script type="text/javascript" src="<?php echo $prefix; ?>../src/remote/json.js"></script>
-<script type="text/javascript" src="<?php echo $prefix; ?>../src/form/select.js"></script>
-<script type="text/javascript" src="<?php echo $prefix; ?>../src/effect/resize.js"></script>
-<script type="text/javascript" src="<?php echo $prefix; ?>../src/layout/renderer.js"></script>
 <script type="text/javascript">
     var w = new ludo.Window({
         title:'LudoDB Integration',
--- js/ludojs.js
+++ js/ludojs.js
@@ -1,4 +1,4 @@
-/* Generated Sat Apr 13 22:19:53 CEST 2013 */
+/* Generated Sat Apr 13 22:24:01 CEST 2013 */
 /************************************************************************************************************
 @fileoverview
 ludoJS - Javascript framework
@@ -21372,6 +21372,41 @@ ludo.tree.Filter = new Class({
         }
         return '';
     }
+});/* ../ludojs/src/ludo-db/factory.js */
+ludo.ludoDB.Factory = new Class({
+    Extends:ludo.Core,
+    ludoDBConfig:undefined,
+
+    recordId:undefined,
+    resource:undefined,
+    url:undefined,
+
+    ludoConfig:function (config) {
+        if (config.id) {
+            this.recordId = config.id;
+            config.id = undefined;
+        }
+        this.parent(config);
+        this.setConfigParams(config, ['url', 'resource']);
+
+        this.ludoDBConfig = config;
+    },
+
+    load:function () {
+        var arguments = [this.resource];
+        if(this.recordId)arguments.push(this.recordId);
+        var req = new ludo.remote.JSON({
+            resource:'LudoJS',
+            url : this.getUrl()
+        });
+        req.addEvent('success', this.loadComplete.bind(this));
+        req.send('form', arguments);
+    },
+
+    loadComplete:function(req){
+        this.fireEvent('load', req.getResponseData());
+    }
+
 });/* ../ludojs/src/data-source/html.js */
 /**
  * Class for remote data source.
