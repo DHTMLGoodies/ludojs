31c45f15d10d0a8a64c76cf2affdc511a868212f
DHTMLGoodies <post@dhtmlgoodies.com>
2013-04-18 18:10:53 +0200

    Compiled with ludoDb component included

--- js/ludojs.js
+++ js/ludojs.js
@@ -1,4 +1,4 @@
-/* Generated Thu Apr 18 17:36:16 CEST 2013 */
+/* Generated Thu Apr 18 18:10:14 CEST 2013 */
 /************************************************************************************************************
 @fileoverview
 ludoJS - Javascript framework
@@ -5849,6 +5849,39 @@ ludo.remote.Message = new Class({
 ludo.remote.ErrorMessage = new Class({
     Extends:ludo.remote.Message,
     messageTypes:['failure','serverError']
+});/* ../ludojs/src/ludo-db/factory.js */
+ludo.ludoDB.Factory = new Class({
+    Extends:ludo.Core,
+    ludoDBConfig:undefined,
+
+    arguments:undefined,
+    resource:undefined,
+    url:undefined,
+
+    ludoConfig:function (config) {
+        this.parent(config);
+        this.setConfigParams(config, ['url', 'resource','arguments']);
+        if(this.arguments && !ludo.util.isArray(this.arguments)){
+            this.arguments = [this.arguments];
+        }
+        this.ludoDBConfig = config;
+    },
+
+    load:function () {
+        var arguments = [this.resource];
+        if(this.arguments)arguments.splice(1, 0, this.arguments);
+        var req = new ludo.remote.JSON({
+            resource:'LudoJS',
+            url : this.getUrl()
+        });
+        req.addEvent('success', this.loadComplete.bind(this));
+        req.send('form', arguments);
+    },
+
+    loadComplete:function(req){
+        this.fireEvent('load', req.getResponseData());
+    }
+
 });/* ../ludojs/src/color/color.js */
 /**
  * Color functions
