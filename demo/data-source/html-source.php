93c9a98ff8ddc00007cded667c9cb707bbc1fc38
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-04-23 01:53:15 +0200

    New html datasource demo

--- demo/data-source/html-source.php
+++ demo/data-source/html-source.php
@@ -1,6 +1,6 @@
 <?php
 $sub = true;
-$pageTitle = 'Color widgets';
+$pageTitle = 'HTML data sources';
 require_once("../includes/demo-header.php");
 ?>
 <body>
@@ -8,13 +8,15 @@ require_once("../includes/demo-header.php");
 <script type="text/javascript">
     var w = new ludo.Window({
         id:'myWindow',
+        title : 'HTML Data sources',
         layout:{
             left:20,top:20,
             width:500,height:400
         },
         css:{
             'background-color' : '#FFF',
-            'overflow-y' : 'auto'
+            'overflow-y' : 'auto',
+            padding:5
         },
         dataSource:{
             type:'dataSource.HTML',
