9564bd1565a22d74eabf93bc6cde4d5208ada7b7
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-04-29 00:16:17 +0200

    Skeleton for chart layout handler

---
+++ src/chart/layout.js
@@ -0,0 +1,10 @@
+/**
+ * Layout manager for items in a chart
+ * @namespace chart
+ * @class Layout
+ * @extends layout.Relative
+ */
+ludo.chart.Layout = new Class({
+    Extends: ludo.layout.Relative
+
+});
