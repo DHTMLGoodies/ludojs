fb5417af4698c835104df433cbeecf611147d151
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-05-03 00:37:48 +0200

    Tests for text box(SVG)

--- src-tests/canvas/TextBoxTest.js
+++ src-tests/canvas/TextBoxTest.js
@@ -13,7 +13,7 @@ TestCase("TestBoxTest", {
 	},
 
 
-	"test should convert html to svg tags": function(){
+	"test should nest children correctly": function(){
 		// given
 		var t = this.getTextBox({
 			attr:{ x:100,y:100,width:100,height:100 },
@@ -24,14 +24,31 @@ TestCase("TestBoxTest", {
 		var tagsToInsert = t.tagsToInsert();
 
 		// then
-		assertEquals(3, tagsToInsert.length);
-
+		assertEquals(1, tagsToInsert.length);
+        assertEquals(3, tagsToInsert[0].children.length);
 	},
 
+    "test should create correct children structure": function(){
+        // given
+        var t = this.getTextBox({
+            attr:{ x:100,y:100,width:100,height:100 },
+            text : 'Normal <b>Bold</b> Normal<br>Second line'
+        });
+
+        // when
+        var tagsToInsert = t.tagsToInsert();
+
+        // then
+        assertEquals('text', tagsToInsert[0].tag);
+        assertEquals('tspan', tagsToInsert[0].children[0].tag);
+        assertEquals('tspan', tagsToInsert[1].children[0].tag);
+        assertEquals('tspan', tagsToInsert[2].children[0].tag);
+        assertEquals('texgt', tagsToInsert[1].tag);
+
+    },
+
 	getTextBox:function(config){
-
 		return new ludo.canvas.TextBox(config);
-
 	}
 
 });
