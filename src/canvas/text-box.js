1559e74edde36b7ba056d4598d42b1522fe43b91
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-05-05 18:40:46 +0200

    Code review and refactoring

--- src/canvas/text-box.js
+++ src/canvas/text-box.js
@@ -5,159 +5,154 @@
  * @class TextBox
  */
 ludo.canvas.TextBox = new Class({
-	Extends:ludo.canvas.Group,
-	tag:'g',
-	text:undefined,
-	css:{
-		'line-height':13
-	},
-	presentationAttributes:['x', 'y', 'width', 'height'],
+    Extends:ludo.canvas.Group,
+    tag:'g',
+    text:undefined,
+    css:{
+        'line-height':13
+    },
+    presentationAttributes:['x', 'y', 'width', 'height'],
+    currentY:0,
 
-	ludoConfig:function (config) {
-		config = config || {};
-		config.attr = this.getAttributes(config);
-		this.parent(config);
-		this.text = config.text;
-		if (config.css)this.css = config.css;
+    tags:{
+        '<br>':'text'
+    },
 
-		this.renderText();
-	},
+    ludoConfig:function (config) {
+        config = config || {};
+        config.attr = this.getAttributes(config);
+        this.parent(config);
+        this.text = config.text;
+        if (config.css)this.css = config.css;
 
-	getAttributes:function (config) {
-		var ret = config.attr || {};
-		var p = this.presentationAttributes;
-		for (var i = 0; i < p.length; i++) {
-			if (config[p[i]] !== undefined) {
-				ret[p[i]] = config[p[i]];
-			}
-		}
-		return ret;
-	},
+        this.renderText();
+    },
 
-	tagsToInsert:function () {
-		var texts = this.getTextParts();
-		return this.toTags(texts);
-	},
+    getAttributes:function (config) {
+        var ret = config.attr || {};
+        var p = this.presentationAttributes;
+        for (var i = 0; i < p.length; i++) {
+            if (config[p[i]] !== undefined) {
+                ret[p[i]] = config[p[i]];
+            }
+        }
+        return ret;
+    },
 
-	getTextParts:function () {
-		var tokens = this.text.match(/(<\/?[a-z]+?>)/gmi);
-		var index = 0;
+    tagsToInsert:function () {
+        var texts = this.getTextParts();
+        return this.toTags(texts);
+    },
 
-		var ret = [];
-		for (var i = 0; i < tokens.length; i++) {
-			var pos = this.text.indexOf(tokens[i], index === 0 ? index : index + 1);
-			if (pos > index) {
-				ret.push(this.text.substr(index, pos - index));
-			}
-			index = pos;
-		}
-		if (index < this.text.length) {
-			ret.push(this.text.substr(index));
-		}
+    getTextParts:function () {
+        var tokens = this.text.match(/(<\/?[a-z]+?>)/gmi);
+        var index = 0;
 
-		return ret;
-	},
+        var ret = [];
+        for (var i = 0; i < tokens.length; i++) {
+            var pos = this.text.indexOf(tokens[i], index === 0 ? index : index + 1);
+            if (pos > index) {
+                ret.push(this.text.substr(index, pos - index));
+            }
+            index = pos;
+        }
+        if (index < this.text.length) {
+            ret.push(this.text.substr(index));
+        }
 
-	toTags:function (items) {
-		var ret = [
-			{
-				tag:'text',
-				properties:{ y:this.getYOfLine() }
-			}
-		];
-		var currentParent = ret[0];
+        return ret;
+    },
 
-		for (var i = 0; i < items.length; i++) {
-			var tag = items[i].match(/<([a-z]+?)>/gi);
-			if (tag)tag = tag[0];
+    toTags:function (items) {
+        var ret = [
+            {
+                tag:'text',
+                properties:{ y:this.getYOfLine() }
+            }
+        ];
+        var currentParent = ret[0];
 
-			var obj = {
-				properties:{}
-			};
+        for (var i = 0; i < items.length; i++) {
+            var tag = items[i].match(/<([a-z]+?)>/gi);
+            if (tag)tag = tag[0];
 
-			switch (tag) {
-				case '<em>':
-				case '<b>':
-					obj.tag = 'tspan';
-					break;
-				case '<br>':
-					obj.tag = 'text';
-					break;
-				default:
-					obj.tag = 'tspan';
-			}
+            var obj = {
+                properties:{}
+            };
+
+            obj.tag = this.tags[tag] ? this.tags[tag] : 'tspan';
 
 
-			obj = this.assignDefaultProperties(obj, tag);
+            obj = this.assignDefaultProperties(obj, tag);
 
 
-			obj.text = this.stripTags(items[i]);
-			if (obj.tag === 'tspan') {
-				currentParent.children = currentParent.children || [];
-				currentParent.children.push(obj);
-			} else {
-				ret.push(obj);
-			}
-			if (obj.tag === 'text') {
-				obj.properties.y = this.getYOfLine();
-				currentParent = obj;
-			}
-		}
+            obj.text = this.stripTags(items[i]);
+            if (obj.tag === 'tspan') {
+                currentParent.children = currentParent.children || [];
+                currentParent.children.push(obj);
+            } else {
+                ret.push(obj);
+            }
+            if (obj.tag === 'text') {
+                obj.properties.y = this.getYOfLine();
+                currentParent = obj;
+            }
+        }
 
-		return ret;
-	},
+        return ret;
+    },
 
-	assignDefaultProperties:function (obj, tag) {
-		if (tag) {
-			var props = this.getDefaultTagProperties();
-			var p = props[tag];
-			if (p) {
-				for (var key in p) {
-					if (p.hasOwnProperty(key))obj.properties[key] = p[key];
-				}
-			}
-		}
-		return obj;
+    assignDefaultProperties:function (obj, tag) {
+        if (tag) {
+            var props = this.getDefaultTagProperties();
+            var p = props[tag];
+            if (p) {
+                for (var key in p) {
+                    if (p.hasOwnProperty(key))obj.properties[key] = p[key];
+                }
+            }
+        }
+        return obj;
 
-	},
-
-	currentY:0,
-	getYOfLine:function () {
-		this.currentY += 15;
-		return this.currentY;
-	},
-
-	setText:function(text){
-		this.empty();
-		this.text = text;
-		this.renderText();
-	},
-
-	renderText:function () {
-		this.renderItems(this.tagsToInsert(), this);
-	},
-
-	renderItems:function (items, parent) {
-		for (var i = 0; i < items.length; i++) {
-			var n = new ludo.canvas.Node(items[i].tag, items[i].properties, items[i].text);
-			parent.adopt(n);
-
-			if (items[i].children) {
-				this.renderItems(items[i].children, n);
-			}
-		}
-	},
+    },
 
 
-	stripTags:function (text) {
-		return text.replace(/<\/?[^>]+?>/g, '');
-	},
+    getYOfLine:function () {
+        this.currentY += 15;
+        return this.currentY;
+    },
 
-	getDefaultTagProperties:function () {
-		return {
-			'<b>':{ 'font-weight':'bold' },
-			'<em>':{ 'font-style':'italic' }
-		};
-	}
+    setText:function (text) {
+        this.empty();
+        this.text = text;
+        this.renderText();
+    },
+
+    renderText:function () {
+        this.renderItems(this.tagsToInsert(), this);
+    },
+
+    renderItems:function (items, parent) {
+        for (var i = 0; i < items.length; i++) {
+            var n = new ludo.canvas.Node(items[i].tag, items[i].properties, items[i].text);
+            parent.adopt(n);
+
+            if (items[i].children) {
+                this.renderItems(items[i].children, n);
+            }
+        }
+    },
+
+
+    stripTags:function (text) {
+        return text.replace(/<\/?[^>]+?>/g, '');
+    },
+
+    getDefaultTagProperties:function () {
+        return {
+            '<b>':{ 'font-weight':'bold' },
+            '<em>':{ 'font-style':'italic' }
+        };
+    }
 
 });
