79225b9a0cd2ec061c5c81f36eee066190413a9c
DHTMLGoodies <post@dhtmlgoodies.com>
2013-06-12 19:41:59 +0200

    Updated demo

--- demo/user-admin/demo.php
+++ demo/user-admin/demo.php
@@ -5,140 +5,148 @@ require_once("../includes/demo-header.php");
 ?>
 <body>
 <script type="text/javascript">
-
-    var v = new ludo.Window({
-        renderTo:document.body,
-        title:'User admin',
-        layout:{
-            type:'linear',
-            orientation:'horizontal',
-            left:20, top:20,
-            width:700, height:500
-        },
-        children:[
-            {
-                type:'grid.Grid',
-                layout:{
-                    width:300
-                },
-                containerCss:{
-                    'border-right':'1px solid #d7d7d7'
-                },
-                resizable:true,
-                dataSource:{
-                    url:'../controller.php',
-                    resource:'LudoJSUsers',
-                    service:'read',
-                    listeners:{
-                        "select":function (record) {
-                            ludo.get('formView').getForm().read(record.id);
-                        }
-                    }
-                },
-                columns:{
-                    'firstname':{
-                        heading:'Firstname',
-                        sortable:true,
-                        movable:true
+    ludo.config.setUrl('../controller.php');
+        window.addEvent('domready', function () {
+            new ludo.Window({
+                    renderTo:document.body,
+                    title:'User admin',
+                    layout:{
+                        type:'linear',
+                        orientation:'horizontal',
+                        left:20, top:20,
+                        width:700, height:500
                     },
-                    'lastname':{
-                        heading:'Lastname',
-                        sortable:true,
-                        movable:true
-                    },
-                    country_name:{
-                        heading:'Country',
-                        movable:true
-                    }
-                }
+                    children:[
+                        {
+                            type:'grid.Grid',
+                            layout:{
+                                width:300
+                            },
+                            containerCss:{
+                                'border-right':'1px solid #d7d7d7'
+                            },
+                            resizable:true,
+                            dataSource:{
+                                resource:'LudoJSUsers',
+                                service:'read',
+                                listeners:{
+                                    "select":function (record) {
+                                        ludo.get('formView').getForm().read(record.id);
+                                    }
+                                }
+                            },
 
-            },
-            {
-                type:'FramedView',
-                title:'Edit user',
-                id:'formView',
-                minimizable:false,
-                containerCss:{
-                    'border':0,
-                    'border-left':'1px solid #d7d7d7'
-                },
-                layout:{
-                    weight:1,
-                    type:'linear',
-                    orientation:'vertical'
-                },
-                form:{
-                    'resource':'LudoJSUser',
-                    listeners:{
-                        'beforeRead':function () {
-                            ludo.get('formView').shim().show('Loading record');
+                            columns:{
+                                'firstname':{
+                                    heading:'Firstname',
+                                    sortable:true,
+                                    movable:true
+                                },
+                                'lastname':{
+                                    heading:'Lastname',
+                                    sortable:true,
+                                    movable:true
+                                },
+                                country_name:{
+                                    heading:'Country',
+                                    movable:true
+                                }
+                            }
+
                         },
-                        'beforeSave':function () {
-                            ludo.get('formView').shim().show('Saving record');
-                        },
-                        'read':function () {
-                            ludo.get('formView').shim().hide();
-                            ludo.get('formView').setTitle('Edit user')
-                        },
-                        'saved':function () {
-                            ludo.get('formView').shim().hide();
-                        },
-                        'new' : function(){
-                            ludo.get('formView').setTitle('New user')
+                        {
+                            type:'FramedView',
+                            title:'Edit user',
+                            id:'formView',
+                            minimizable:false,
+                            containerCss:{
+                                'border':0,
+                                'border-left':'1px solid #d7d7d7'
+                            },
+                            layout:{
+                                weight:1,
+                                type:'linear',
+                                orientation:'vertical'
+                            },
+                            form:{
+                                'resource':'LudoJSUser',
+                                listeners:{
+                                    'beforeRead':function () {
+                                        ludo.get('formView').shim().show('Loading record');
+                                    },
+                                    'beforeSave':function () {
+                                        ludo.get('formView').shim().show('Saving record');
+                                    },
+                                    'read':function () {
+                                        ludo.get('formView').shim().hide();
+                                        ludo.get('formView').setTitle('Edit user')
+                                    },
+                                    'afterRequest':function () {
+                                        ludo.get('formView').shim().hide();
+                                    },
+                                    'new' : function(){
+                                        ludo.get('formView').setTitle('New user')
+                                    }
+                                }
+                            },
+                            children:[
+                                { type:'form.Text', inlineLabel:'First name', name:'firstname', required:true },
+                                { type:'form.Text', inlineLabel:'Last name', name:'lastname', required:true },
+                                { type:'form.Textarea', inlineLabel:'Address', name:'address', required:true },
+                                {
+                                    type:'form.Select',
+                                    dataSource:{
+                                        'resource':'LudoJSCountries',
+                                        'service':'read'
+                                    },
+                                    valueKey:'id',
+                                    textKey:'name',
+                                    name:'country',
+                                    emptyItem:{
+                                        'id':'',
+                                        'name':'Select country'
+                                    }
+                                },
+                                {
+                                    type:'form.RadioGroup',
+                                    labelWidth : 50,
+                                    id:'gender',
+                                    name:'gender',
+                                    label:'Gender',
+                                    options:[
+                                        { value:'f', text:'Female' },
+                                        { value:'m', text:'Male' }
+                                    ],
+                                    value:'f'
+                                },
+                                {
+                                    type:'remote.ErrorMessage',
+                                    listenTo:['User/save'],
+                                    layout:{
+                                        height:30
+                                    }
+                                }
+                            ]
                         }
-                    }
-                },
-                children:[
-                    { type:'form.Text', inlineLabel:'First name', name:'firstname', required:true },
-                    { type:'form.Text', inlineLabel:'Last name', name:'lastname', required:true },
-                    { type:'form.Textarea', inlineLabel:'Address', name:'address', required:true },
-                    {
-                        type:'form.Select',
-                        dataSource:{
-                            'resource':'LudoJSCountries',
-                            'service':'read'
+                    ],
+
+                    buttons:[
+                        {
+                            type:'form.Button', value:'New',
+                            listeners:{
+                                'click':function () {
+                                    ludo.get('formView').getForm().newRecord();
+                                }
+                            }
                         },
-                        valueKey:'id',
-                        textKey:'name',
-                        name:'country',
-                        emptyItem:{
-                            'id':'',
-                            'name':'Select country'
+                        {
+                            type:'form.SubmitButton', applyTo:'formView', value:'Save'
                         }
-                    },
-                    {
-                        type:'form.RadioGroup',
-                        labelWidth : 50,
-                        id:'gender',
-                        name:'gender',
-                        label:'Gender',
-                        options:[
-                            { value:'f', text:'Female' },
-                            { value:'m', text:'Male' }
-                        ],
-                        value:'f'
-                    }
-                ]
-            }
-        ],
-
-        buttons:[
-            {
-                type:'form.Button', value:'New',
-                listeners:{
-                    'click':function () {
-                        ludo.get('formView').getForm().newRecord();
-                    }
-                }
-            },
-            {
-                type:'form.SubmitButton', applyTo:'formView', value:'Save'
-            }
-        ]
+                    ]
 
 
-    });
-
+                });
+        });
 </script>
 </body>
 </html>
