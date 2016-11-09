c8e3ba23e94ac34e399c5287a9909c0f59a4577b
DHTMLGoodies <post@dhtmlgoodies.com>
2016-11-09 20:53:38 +0100

    Before renaming ludo-view-container

--- js/ludojs.js
+++ js/ludojs.js
@@ -1,7 +1,7 @@
-/* Generated Wed Nov 9 19:06:12 CET 2016 */
+/* Generated Wed Nov 9 20:24:21 CET 2016 */
 /************************************************************************************************************
 @fileoverview
-ludoJS - Javascript framework, 1.1.29
+ludoJS - Javascript framework, 1.1.33
 Copyright (C) 2012-2016  ludoJS.com, Alf Magne Kalleland
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -5783,196 +5783,194 @@ ludo.layout.TextBox = new Class({
     }
 });/* ../ludojs/src/layout/resizer.js */
 ludo.layout.Resizer = new Class({
-	Extends:ludo.Core,
-	layout:{},
-	orientation:undefined,
-	view:undefined,
-	dd:undefined,
-	pos:undefined,
-	isActive:false,
-	hidden:false,
+    Extends: ludo.Core,
+    layout: {},
+    orientation: undefined,
+    view: undefined,
+    dd: undefined,
+    pos: undefined,
+    isActive: false,
+    hidden: false,
 
-	ludoConfig:function (config) {
-		this.parent(config);
-        this.setConfigParams(config, ['orientation','view','layout','pos','hidden']);
-		this.createDOM(config.renderTo);
-		this.addViewEvents();
-		this.createDragable();
-        if(this.hidden)this.hide();
-	},
+    ludoConfig: function (config) {
+        this.parent(config);
+        this.setConfigParams(config, ['orientation', 'view', 'layout', 'pos', 'hidden']);
+        this.createDOM(config.renderTo);
+        this.addViewEvents();
+        this.createDragable();
+        if (this.hidden)this.hide();
+    },
 
-	createDOM:function(renderTo){
-		this.el = $('<div>');
-		this.el.on('mouseenter', this.enterResizer.bind(this));
-		this.el.on('mouseleave', this.leaveResizer.bind(this));
-		this.el.addClass("ludo-resize-handle");
-		this.el.addClass('ludo-resize-handle-' + ((this.orientation === 'horizontal') ? 'col' : 'row'));
-		this.el.addClass('ludo-layout-resize-' + ((this.orientation === 'horizontal') ? 'col' : 'row'));
+    createDOM: function (renderTo) {
+        this.el = $('<div>');
+        this.el.on('mouseenter', this.enterResizer.bind(this));
+        this.el.on('mouseleave', this.leaveResizer.bind(this));
+        this.el.addClass("ludo-resize-handle");
+        this.el.addClass('ludo-resize-handle-' + ((this.orientation === 'horizontal') ? 'col' : 'row'));
+        this.el.addClass('ludo-layout-resize-' + ((this.orientation === 'horizontal') ? 'col' : 'row'));
 
-		this.el.css({
-			cursor: (this.orientation == 'horizontal' ? 'w-resize' : 's-resize'),
-			zIndex : 100000
-		});
+        this.el.css({
+            cursor: (this.orientation == 'horizontal' ? 'ew-resize' : 'ns-resize'),
+            zIndex: 100000
+        });
 
-		renderTo.append(this.el);
+        renderTo.append(this.el);
 
-	},
+    },
 
-	enterResizer:function(){
-		if(!this.isActive){
-			this.el.css('z-index', parseInt(this.el.css('z-index')) + 1);
-			this.el.addClass('ludo-resize-handle-active');
-		}
-	},
+    enterResizer: function () {
+        if (!this.isActive) {
+            this.el.css('z-index', parseInt(this.el.css('z-index')) + 1);
+            this.el.addClass('ludo-resize-handle-active');
+        }
+    },
 
-	leaveResizer:function(){
-		if(!this.isActive){
-			this.el.css('z-index', parseInt(this.el.css('z-index')) - 1);
-			this.el.removeClass('ludo-resize-handle-active');
-		}
-	},
-	createDragable:function(){
-		this.dd = new ludo.effect.Drag({
-			directions : this.orientation == 'horizontal' ? 'X' : 'Y'
-		});
-		this.dd.addEvent('before', this.beforeDrag.bind(this));
-		this.dd.addEvent('end', this.endDrag.bind(this));
-		this.dd.add(this.el);
-	},
+    leaveResizer: function () {
+        if (!this.isActive) {
+            this.el.css('z-index', parseInt(this.el.css('z-index')) - 1);
+            this.el.removeClass('ludo-resize-handle-active');
+        }
+    },
+    createDragable: function () {
+        this.dd = new ludo.effect.Drag({
+            directions: this.orientation == 'horizontal' ? 'X' : 'Y'
+        });
+        this.dd.addEvent('before', this.beforeDrag.bind(this));
+        this.dd.addEvent('end', this.endDrag.bind(this));
+        this.dd.add(this.el);
+    },
 
-	beforeDrag:function(){
-		this.dd.setMinX(30);
-		this.isActive = true;
-		this.el.removeClass( 'ludo-resize-handle-active');
-		this.el.addClass('ludo-resize-handle-active');
-		this.fireEvent('before', [this, this.view]);
-		this.fireEvent('startResize');
-	},
+    beforeDrag: function () {
+        this.dd.setMinX(30);
+        this.isActive = true;
+        this.el.removeClass('ludo-resize-handle-active');
+        this.el.addClass('ludo-resize-handle-active');
+        this.fireEvent('before', [this, this.view]);
+        this.fireEvent('startResize');
+    },
 
-	setMinWidth:function(x){
-		if(this.pos === 'left'){
-			var el = this.view.getEl();
-			this.dd.setMaxX(el.offsetLeft + el.width() - x);
-		}else{
-			this.dd.setMinX(this.view.getEl().position().left + x);
-		}
-	},
+    setMinWidth: function (x) {
+        if (this.pos === 'left') {
+            var el = this.view.getEl();
+            this.dd.setMaxX(el.position().left + el.width() - x);
+        } else {
+            this.dd.setMinX(this.view.getEl().position().left + x);
+        }
+    },
 
-	setMaxWidth:function(x){
-		var el = this.view.getEl();
-		if(this.pos === 'right'){
-			this.dd.setMaxX(el.position().left + x);
-			console.log(x);
-			
-		}else{
-			var pos = 0;
-			if(this.layout.affectedSibling){
-				pos = this.layout.affectedSibling.getEl().position().left + 10;
-			}
-			this.dd.setMinX(Math.max(pos, el.offsetLeft + el.width() - x));
-		}
-	},
+    setMaxWidth: function (x) {
+        var el = this.view.getEl();
+        if (this.pos === 'right') {
+            this.dd.setMaxX(el.position().left + x);
+        } else {
+            var pos = 0;
+            if (this.layout.affectedSibling) {
+                pos = this.layout.affectedSibling.getEl().position().left + 10;
+            }
+            this.dd.setMinX(Math.max(pos, el.position().left + el.width() - x));
+        }
+    },
 
-	setMinHeight:function(y){
-		if(this.pos === 'above'){
-			var el = this.view.getEl();
-			this.dd.setMaxY(el.position().top + el.height() - y);
-		}else{
-			this.dd.setMinY(this.view.getEl().position().top + y);
-		}
+    setMinHeight: function (y) {
+        if (this.pos === 'above') {
+            var el = this.view.getEl();
+            this.dd.setMaxY(el.position().top + el.height() - y);
+        } else {
+            this.dd.setMinY(this.view.getEl().position().top + y);
+        }
 
-	},
+    },
 
-	setMaxHeight:function(y){
-		var el = this.view.getEl();
-		if(this.pos === 'below'){
-			this.dd.setMaxY(el.position().top + y);
-		}else{
-			var pos = 10;
-			if(this.layout.affectedSibling){
-				pos = this.layout.affectedSibling.getEl().position().top + 10;
-			}
-			this.dd.setMinY(Math.max(pos, el.position().top + el.height() - y));
-		}
-	},
+    setMaxHeight: function (y) {
+        var el = this.view.getEl();
+        if (this.pos === 'below') {
+            this.dd.setMaxY(el.position().top + y);
+        } else {
+            var pos = 10;
+            if (this.layout.affectedSibling) {
+                pos = this.layout.affectedSibling.getEl().position().top + 10;
+            }
+            this.dd.setMinY(Math.max(pos, el.position().top + el.height() - y));
+        }
+    },
 
-	endDrag:function(dragged, dd){
-		this.el.removeClass( 'ludo-resize-handle-over');
-		this.el.removeClass( 'ludo-resize-handle-active');
-		var change = this.orientation === 'horizontal' ? dd.getDraggedX() : dd.getDraggedY();
-		if(this.pos === 'left' || this.pos === 'above'){
-			change *= -1;
-		}
-		this.fireEvent('resize', change);
-		this.fireEvent('stopResize');
-		this.isActive = false;
-	},
+    endDrag: function (dragged, dd) {
+        this.el.removeClass('ludo-resize-handle-over');
+        this.el.removeClass('ludo-resize-handle-active');
+        var change = this.orientation === 'horizontal' ? dd.getDraggedX() : dd.getDraggedY();
+        if (this.pos === 'left' || this.pos === 'above') {
+            change *= -1;
+        }
+        this.fireEvent('resize', change);
+        this.fireEvent('stopResize');
+        this.isActive = false;
+    },
 
-	getEl:function(){
-		return this.el;
-	},
+    getEl: function () {
+        return this.el;
+    },
 
-	addViewEvents:function () {
-		this.view.addEvent('maximize', this.show.bind(this));
-		this.view.addEvent('expand', this.show.bind(this));
-		this.view.addEvent('minimize', this.hide.bind(this));
-		this.view.addEvent('collapse', this.hide.bind(this));
-		this.view.addEvent('hide', this.hide.bind(this));
-		this.view.addEvent('show', this.show.bind(this));
-	},
+    addViewEvents: function () {
+        this.view.addEvent('maximize', this.show.bind(this));
+        this.view.addEvent('expand', this.show.bind(this));
+        this.view.addEvent('minimize', this.hide.bind(this));
+        this.view.addEvent('collapse', this.hide.bind(this));
+        this.view.addEvent('hide', this.hide.bind(this));
+        this.view.addEvent('show', this.show.bind(this));
+    },
 
-	show:function(){
-		this.el.css('display', '');
-		this.hidden = false;
-	},
+    show: function () {
+        this.el.css('display', '');
+        this.hidden = false;
+    },
 
-	hide:function(){
-		this.hidden = true;
-		this.el.css('display', 'none');
-	},
+    hide: function () {
+        this.hidden = true;
+        this.el.css('display', 'none');
+    },
 
-	getWidth:function(){
-		return this.hidden ? 0 : 5;
-	},
+    getWidth: function () {
+        return this.hidden ? 0 : 5;
+    },
 
-	getHeight:function(){
-		return this.hidden ? 0 : 5;
-	},
+    getHeight: function () {
+        return this.hidden ? 0 : 5;
+    },
 
-	resize:function(config){
-		this.el.css({
-			left:'', top:'',right:'',bottom:''
-		});
+    resize: function (config) {
+        this.el.css({
+            left: '', top: '', right: '', bottom: ''
+        });
 
 
-		if(config.width !== undefined && config.width > 0)this.el.css('width', config.width);
-		if(config.height !== undefined && config.height > 0)this.el.css('height', (config.height - ludo.dom.getMBPH(this.el)));
-		if(config.left !== undefined)this.el.css('left', config.left);
-		if(config.top !== undefined)this.el.css('top', config.top);
-		if(config.bottom !== undefined)this.el.css('bottom', config.bottom);
-		if(config.right !== undefined)this.el.css('right', config.right);
-	},
+        if (config.width !== undefined && config.width > 0)this.el.css('width', config.width);
+        if (config.height !== undefined && config.height > 0)this.el.css('height', (config.height - ludo.dom.getMBPH(this.el)));
+        if (config.left !== undefined)this.el.css('left', config.left);
+        if (config.top !== undefined)this.el.css('top', config.top);
+        if (config.bottom !== undefined)this.el.css('bottom', config.bottom);
+        if (config.right !== undefined)this.el.css('right', config.right);
+    },
 
-	getParent:function(){
+    getParent: function () {
 
-	},
-	setParentComponent:function(){
+    },
+    setParentComponent: function () {
 
-	},
-	isVisible:function(){
-		return !this.hidden;
-	},
-	isHidden:function(){
-		return this.hidden;
-	},
+    },
+    isVisible: function () {
+        return !this.hidden;
+    },
+    isHidden: function () {
+        return this.hidden;
+    },
 
-	hasChildren:function(){
-		return false;
-	},
+    hasChildren: function () {
+        return false;
+    },
 
-	isFormElement:function(){
-		return false;
-	}
+    isFormElement: function () {
+        return false;
+    }
 });/* ../ludojs/src/layout/base.js */
 /**
 * Base class for ludoJS layouts
@@ -15131,7 +15129,7 @@ ludo.layout.Table = new Class({
     },
 
     resize: function () {
-        console.log('resize table');
+
         var c = this.view.children;
         var curCellIndex = 0;
         for (var i = 0; i < c.length; i++) {
@@ -16548,10 +16546,10 @@ ludo.layout.Relative = new Class({
 	onCreate:function () {
 		this.parent();
 		this.view.getBody().css('position', 'relative');
+
 	},
 
 	resize:function () {
-
 		if (this.children === undefined) {
 			this.prepareResize();
 		}
@@ -17075,14 +17073,26 @@ ludo.layout.Relative = new Class({
 	beforeResize:function(resize, child){
 
 		if(resize.orientation === 'horizontal'){
-			console.log(child.layout.maxWidth);
-			resize.setMinWidth(child.layout.minWidth || 10);
-			resize.setMaxWidth(child.layout.maxWidth || this.view.getBody().width());
+			var min = this.toPixels(child.layout.minWidth || 10);
+			resize.setMinWidth(min);
+			var max = child.layout.maxWidth || this.view.getBody().width();
+			max = this.toPixels(max, true);
+			resize.setMaxWidth(max);
 		}else{
-			resize.setMinHeight(child.layout.minHeight || 10);
+			var minHeight = this.toPixels(child.layout.minHeight || 10);
+			resize.setMinHeight(minHeight);
 			resize.setMaxHeight(child.layout.maxHeight || this.view.getBody().height());
 		}
 	},
+
+	toPixels:function(value, isWidth){
+		if(jQuery.type(value) == "string" && value.indexOf('%') >0){
+			var val = parseInt(value);
+			return val / 100 * (isWidth ? this.viewport.width : this.viewport.height);
+		}
+		return value;
+	},
+
     /**
      * Return layout config for a resize handle
      * @function getResizerLayout
