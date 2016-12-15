5972f605a56e046444c7a6e0afb69660b9028d1e
DHTMLGoodies <post@dhtmlgoodies.com>
2016-12-15 02:11:18 +0100

    Transformation demo

--- demo/canvas/transformation.php
+++ demo/canvas/transformation.php
@@ -69,13 +69,16 @@ require_once("../includes/demo-header.php");
      */
     var rect2 = c.$('rect', { x : 0, y: 0, width: 120, height: 100});
     rect2.css({ fill : '#669900' });
+    rect2.css({ 'stroke-width' : 3, stroke: '#fff'});
     c.append(rect2);
 
     rect2.translate(200,250);
+    rect2.attr("stroke-linejoin", "round");
+    rect2.attr("stroke-linecap", "round");
 
     function animate(){
         rect2.animate({
-            rotate: [360, 60, 50 ]
+            rotate: [450, 60, 50 ]
         }, 4000, ludo.canvas.easing.bounce, animate);
     }
 
@@ -94,6 +97,10 @@ require_once("../includes/demo-header.php");
         'stroke-width' : 5
     });
     c.append(ellipse);
+
+
+
+
 
     // First animation function, animates x radius
     function animateEllipseX(){
@@ -137,6 +144,56 @@ require_once("../includes/demo-header.php");
     rect4.translate(-25,-25);
     rect4.setScale(1.5,1.5);
 
+
+    // Animation of a path
+    var path = c.$('path');
+
+
+    var topLeft = "0 0";
+    var topRight = "200,0";
+    var bottomRight = "200 60";
+    var bottomLeft = "0 60";
+    var rightCenter = "200 30";
+    var leftCenter = "0 30";
+
+    var arrowTopLeft = "0 -30";
+    var arrowLeftCenter = "-60 30";
+    var arrowBottomLeft ="0 90";
+
+    var arrowTopRight = "200 -30";
+    var arrowRightCenter = "260 30";
+    var arrowBottomRight = "200 90";
+
+    // Constructing two paths with equal number of points
+    var firstArrow = [topLeft, topRight, arrowTopRight, arrowRightCenter, arrowBottomRight, bottomRight, bottomLeft, bottomLeft, leftCenter, topLeft, topLeft];
+    var secondArrow = [topLeft, topRight, topRight, rightCenter, bottomRight, bottomRight, bottomLeft, arrowBottomLeft, arrowLeftCenter, arrowTopLeft, topLeft]
+    var arrow = "M" + firstArrow.join(" L ");
+    var arrow2 = "M" + secondArrow.join(" L ");
+
+
+    path.set("d", arrow);
+    path.css("fill", "#006064");
+    path.css("stroke", "#fff");
+    path.css("stroke-width", 4);
+    path.setTranslate(400,320);
+    path.attr("stroke-linejoin", "round");
+    path.attr("stroke-linecap", "round");
+
+    c.append(path);
+
+    function animatePath(){
+        path.animate({
+            "d": arrow2
+        }, 1500, ludo.canvas.easing.outSine, animatePath2)
+    }
+
+    function animatePath2(){
+        path.animate({
+            "d": arrow
+        }, 2500, ludo.canvas.easing.elastic, animatePath)
+    }
+
+    animatePath();
 
 
 
