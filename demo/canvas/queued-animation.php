1b947f34cddff7f5cf8c05492ea072c3d9b66d19
DHTMLGoodies <post@dhtmlgoodies.com>
2016-12-19 17:33:58 +0100

    animation queuing

--- demo/canvas/queued-animation.php
+++ demo/canvas/queued-animation.php
@@ -29,51 +29,53 @@ require_once("../includes/demo-header.php");
     // Get reference to SVG surface
     var c = ludo.$('demoView').getCanvas();
 
-    var end = c.$('circle', {cx: 0, cy: 0, r: 10, 'fill': '#f00'});
-    c.append(end);
-    end.setTranslate(600, 600);
 
-    var c2 = c.$('circle', {cx: 0, cy: 0, r: 10});
-
+    var c2 = c.$('circle', {cx: 0, cy: 0, r: 10, fill: '#00695C'});
     c.append(c2);
 
 
     c2.setTranslate(100, 100);
-    
 
-    // Example of queued animation
-    c2.animate({
-            translate: ['+500', '+500'],
-            fill: '#669900',
-            r: 20
-        }, {
-            duration: 500
-        }
-    ).animate({
-            translate: ['+100'],
-            r:5
-        }, {
-            duration: 500
-        }
-    ).animate({
-        translate:['+0','-100'],
-        r: 15,
-        fill: '#689F38'
-    }).animate({
-        translate:['-200','-300'],
-        r:35,
-        fill: '#FBC02D'
-    }, { duration: 1200 }).animate({
-        translate:['-200','400']
-    }).animate({
-        'r' : 200,
-        'fill': '#00695C',
-        'fill-opacity':0
-    },{ duration: 1500 }).animate({
-        'r' : 10,
-        translate:[100,100],
-        'fill-opacity':1
-    },{ duration: 1500 });
+    // Wrap all the queued animations in a function which is called recursively
+    function animate(){
+        // Example of queued animation
+        c2.animate({
+                translate: ['+500', '+500'],
+                fill: '#669900',
+                r: 20
+            }, {
+                duration: 1200,
+                easing:ludo.canvas.easing.bounce
+            }
+        ).animate({
+                translate: ['+100'],
+                r:5
+            }, {
+                duration: 500
+            }
+        ).animate({
+            translate:['+0','-100'],
+            r: 15,
+            fill: '#689F38'
+        }).animate({
+            translate:['-200','-300'],
+            r:35,
+            fill: '#FBC02D'
+        }, { duration: 1200 }).animate({
+            translate:['-200','400']
+        }).animate({
+            'r' : 400,
+            'fill': '#00695C',
+            'fill-opacity':0.4
+        },{ duration: 1500 }).animate({
+            'r' : 10,
+            translate:[100,100],
+            'fill-opacity':1
+        },{ duration: 1500, complete:animate });
+    }
+
+
+    animate();
 
 
 
